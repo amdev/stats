a0e5b6dd42b315abfac3fe6b76eba4f721551934
Panya  Saraphi <elect.tu@gmail.com>
2020-06-30 21:30:56 +0700

    add draw games

--- build/index.js
+++ build/index.js
@@ -16,13 +16,17 @@ var homeWin = 'H';
 var awayWin = 'A';
 var isDraw = 'D';
 var manuWins = 0;
+var drawMatch = 0;
 for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
     var match = matches_1[_i];
-    if (match[1] === 'Man United' && match[5] === 'H') {
+    if (match[1] === 'Man United' && match[5] === homeWin) {
         manuWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
+    else if (match[2] === 'Man United' && match[5] === awayWin) {
         manuWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === isDraw) {
+        drawMatch++;
     }
 }
-console.log("Man United wons " + manuWins + " games");
+console.log("Man United wons " + manuWins + " games and draw " + drawMatch + " games.");
--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,16 @@ const awayWin = 'A';
 const isDraw = 'D';
 
 let manuWins = 0;
+let drawMatch = 0;
 
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === homeWin) {
     manuWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === awayWin) {
     manuWins++;
+  } else if (match[2] === 'Man United' && match[5] === isDraw) {
+    drawMatch++;
   }
 }
 
-console.log(`Man United wons ${manuWins} games`);
+console.log(`Man United wons ${manuWins} games and draw ${drawMatch} games.`);
